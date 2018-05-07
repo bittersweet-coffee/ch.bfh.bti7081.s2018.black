8ac4d61c174eb9ebd78fe7d372b3cdd1c07afcf2
Michael Hofer <michael.hofer@adfinis-sygroup.ch>
2018-05-07 03:43:08 +0200

    First working Main navigation, introduce Navigator component and switch to custom theme

--- src/main/java/ch/bfh/bti7081/s2018/black/pms/Main.java
+++ src/main/java/ch/bfh/bti7081/s2018/black/pms/Main.java
@@ -4,13 +4,15 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+
+import ch.bfh.bti7081.s2018.black.pms.presentor.*;
+import ch.bfh.bti7081.s2018.black.pms.view.*;
+import ch.bfh.bti7081.s2018.black.pms.model.*;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -19,25 +21,22 @@ import com.vaadin.ui.VerticalLayout;
  * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
  * overridden to add component to the user interface and initialize non-component functionality.
  */
-@Theme("mytheme")
+@Theme("pms")
 public class Main extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+    	MainViewImpl mainView = new MainViewImpl();
+    	new MainPresentor(mainView);
+    	
+    	AddictionModel addictionModel = new AddictionModel("Drugs", "Lorem Ipsum");
+    	
+        VerticalLayout mainLayout = new VerticalLayout(mainView);
+        mainLayout.setSizeFull();
+        setContent(mainLayout);
 
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it still works good!"));
-        });
-        
-        layout.addComponents(name, button);
-        
-        setContent(layout);
+    	Navigator navigator = new Navigator(this, this);
+    	navigator.addView("", mainView);
     }
 
     @WebServlet(urlPatterns = "/*", name = "MainServlet", asyncSupported = true)
--- src/main/java/ch/bfh/bti7081/s2018/black/pms/view/MainViewImpl.java
+++ src/main/java/ch/bfh/bti7081/s2018/black/pms/view/MainViewImpl.java
@@ -1,39 +1,62 @@
 package ch.bfh.bti7081.s2018.black.pms.view;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.ExternalResource;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.ui.CustomComponent;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Link;
+import com.vaadin.ui.Notification;
 
-public class MainViewImpl extends CustomComponent implements MainView, ClickListener {
-	
-	private List<MainViewListener> listeners = new ArrayList<MainViewListener>();
+public class MainViewImpl extends CustomComponent implements View, MainView {
 	
 	public MainViewImpl() {
-        final VerticalLayout layout = new VerticalLayout();
+        final GridLayout tileGrid = new GridLayout(3, 2);
+        tileGrid.addStyleName("main-tile-navigation");
         
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+        final Link patientLink = new Link("Patient Managament", new ExternalResource("/patient"));
+        patientLink.setDescription("Patient Management");
+        patientLink.setIcon(new ThemeResource("../pms/img/patient.svg"));
 
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it still works good!"));
-        });
+        final Link agendaLink= new Link("Agenda Managament", new ExternalResource("/agenda"));
+        agendaLink.setDescription("Agenda Management");
+        agendaLink.setIcon(new ThemeResource("../pms/img/patient.svg"));
+
+        final Link clinicLink = new Link("Clinic Managament", new ExternalResource("/clinic"));
+        clinicLink.setDescription("Clinic Management");
+        clinicLink.setIcon(new ThemeResource("../pms/img/patient.svg"));
+
+        final Link medicamentLink= new Link("Medicament Managament", new ExternalResource("/medicament"));
+        medicamentLink.setDescription("Medicament Management");
+        medicamentLink.setIcon(new ThemeResource("../pms/img/patient.svg"));
+
+        final Link reportLink= new Link("Report Managament", new ExternalResource("/report"));
+        reportLink.setDescription("Report Management");
+        reportLink.setIcon(new ThemeResource("../pms/img/patient.svg"));
+
+        final Link addictionLink = new Link("Addiction Managament", new ExternalResource("/addiction"));
+        addictionLink.setDescription("Addiction Management");
+        addictionLink.setIcon(new ThemeResource("../pms/img/patient.svg"));
         
-        layout.addComponents(name, button);
+        tileGrid.addComponent(patientLink, 0, 0);
+        tileGrid.addComponent(agendaLink, 1, 0);
+        tileGrid.addComponent(clinicLink, 2, 0);
+        tileGrid.addComponent(medicamentLink, 0, 1);
+        tileGrid.addComponent(reportLink, 1, 1);
+        tileGrid.addComponent(addictionLink, 2, 1);
+        
+        setCompositionRoot(tileGrid);
 	}
 
 	public void addListener(MainViewListener listener) {
-		listeners.add(listener);
 	}
 
-	public void buttonClick(ClickEvent event) {
+	public void navigationClick() {
 	}
+
+    @Override
+    public void enter(ViewChangeEvent event) {
+        Notification.show("Hello World");
+    }
 }
