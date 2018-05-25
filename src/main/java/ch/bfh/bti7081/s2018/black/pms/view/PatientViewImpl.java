b4762c07240003398399907758905cf6b75f9a72
Antonio Musardo <antonio.musardo94@gmx.ch>
2018-05-25 12:59:13 +0200

    classes from util package commented and class JpaDemo deleted

--- src/main/java/ch/bfh/bti7081/s2018/black/pms/view/PatientViewImpl.java
+++ src/main/java/ch/bfh/bti7081/s2018/black/pms/view/PatientViewImpl.java
@@ -2,12 +2,7 @@ package ch.bfh.bti7081.s2018.black.pms.view;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Label;
-
-import ch.bfh.bti7081.s2018.black.pms.model.UserModel;
-import ch.bfh.bti7081.s2018.black.pms.util.JpaDemo;
-import ch.bfh.bti7081.s2018.black.pms.util.JpaUtility;
 
 public class PatientViewImpl extends PmsCustomComponent implements View, PatientView {
 
