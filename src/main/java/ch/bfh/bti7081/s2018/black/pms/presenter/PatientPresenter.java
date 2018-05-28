a72c62c029f07de6eba0fa8ab130db28189a9dd9
Antonio Musardo <antonio.musardo94@gmx.ch>
2018-05-28 19:15:53 +0200

    PatientPresenter class extended

--- src/main/java/ch/bfh/bti7081/s2018/black/pms/presenter/PatientPresenter.java
+++ src/main/java/ch/bfh/bti7081/s2018/black/pms/presenter/PatientPresenter.java
@@ -1,17 +1,29 @@
 package ch.bfh.bti7081.s2018.black.pms.presenter;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+import ch.bfh.bti7081.s2018.black.pms.model.AddictionModel;
 import ch.bfh.bti7081.s2018.black.pms.model.PatientItem;
 import ch.bfh.bti7081.s2018.black.pms.model.PatientModel;
+import ch.bfh.bti7081.s2018.black.pms.util.JpaDataAccessObject;
+import ch.bfh.bti7081.s2018.black.pms.util.JpaUtility;
 import ch.bfh.bti7081.s2018.black.pms.view.PatientView;
 
 public class PatientPresenter implements PatientView.PatientViewListener{
 	
 	private PatientView view;
 	private PatientModel model;
+	private List<PatientModel> patientModelList;
+	private List<String> patientfirstnameList = new LinkedList<>();
+	private List<String> patientlastnameList = new LinkedList<>();
+
 
 	public PatientPresenter(PatientView view, PatientModel model) {
 		this.view = view;
 		this.model = model;
+		this.fillPatientList();
 	}
 
 	@Override
@@ -19,4 +31,26 @@ public class PatientPresenter implements PatientView.PatientViewListener{
 		
 	}
 
+	public void fillPatientList() {
+		JpaUtility transaction = new JpaUtility();
+		JpaDataAccessObject objects = new JpaDataAccessObject(transaction);
+		this.patientModelList = objects.findAll(PatientModel.class);
+     	
+		for (PatientModel patient : this.patientModelList) {
+     		this.patientfirstnameList.add(patient.getFirstname());
+     		this.patientlastnameList.add(patient.getLastname());
+     	}
+	}
+	
+	@Override
+	public void selectListChanged(String firstName, String lastName) {
+		Optional<PatientModel> optionalPatient = this.patientModelList.stream()
+			.filter(patient -> patient.getFirstname().equals(firstName))
+			.findAny();
+			
+		if(optionalPatient.isPresent()) {
+			this.view.setListDesc(optionalPatient.get().getDescription());
+			this.view.setListSymptoms(optionalPatient.get().getSymptomsAsString());
+		}
+	}
 }
