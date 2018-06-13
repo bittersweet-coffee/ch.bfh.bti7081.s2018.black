d65c953a898176a10fa1890d59b27e2de606aaf7
bielc1 <cedricjosef.bielmann@bfh.students.ch>
2018-06-13 23:27:50 +0200

    added appointments on the report

--- src/main/java/ch/bfh/bti7081/s2018/black/pms/presenter/PdfSource.java
+++ src/main/java/ch/bfh/bti7081/s2018/black/pms/presenter/PdfSource.java
@@ -5,6 +5,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 import com.itextpdf.io.image.ImageData;
 import com.itextpdf.io.image.ImageDataFactory;
@@ -20,8 +22,8 @@ import com.itextpdf.layout.element.Table;
 import com.vaadin.server.StreamResource.StreamSource;
 
 import ch.bfh.bti7081.s2018.black.pms.model.AddictionModel;
+import ch.bfh.bti7081.s2018.black.pms.model.AppointmentModel;
 import ch.bfh.bti7081.s2018.black.pms.model.DoctorModel;
-import ch.bfh.bti7081.s2018.black.pms.model.DrugModel;
 import ch.bfh.bti7081.s2018.black.pms.model.PatientDrugModel;
 import ch.bfh.bti7081.s2018.black.pms.model.PatientItem;
 import ch.bfh.bti7081.s2018.black.pms.model.PatientModel;
@@ -29,21 +31,22 @@ import ch.bfh.bti7081.s2018.black.pms.persistence.JpaDataAccessObject;
 import ch.bfh.bti7081.s2018.black.pms.persistence.JpaUtility;
 
 public class PdfSource implements StreamSource {
+	
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
 
     private ByteArrayOutputStream bof;
     private PatientModel patientModel;
 
     public PdfSource() {
         bof = new ByteArrayOutputStream();
-        PdfWriter writer = new PdfWriter(bof);
-        PdfDocument pdfDoc = new PdfDocument(writer);
-        Document reportpdf = new Document(pdfDoc);
     }
     
     public PdfSource(PatientItem patientItem) throws MalformedURLException {
     	JpaUtility transaction = new JpaUtility();
 		JpaDataAccessObject objects = new JpaDataAccessObject(transaction);
 		patientModel = (PatientModel) objects.byid(PatientModel.class, patientItem.getId());
+		
+		ArrayList<AppointmentModel> appointmentModel = new ArrayList<>(patientItem.getModel().getAppointments());
 		
         bof = new ByteArrayOutputStream();
         
@@ -100,6 +103,7 @@ public class PdfSource implements StreamSource {
         doc.add(addresstable);      
         
         Paragraph pAddiction = new Paragraph("Addictions:");
+        pAddiction.setBold();
         List lAddiction = new List();
         for (AddictionModel am : patientModel.getAddictions()) {
         	lAddiction.add(am.getName());
@@ -108,20 +112,46 @@ public class PdfSource implements StreamSource {
         doc.add(lAddiction);
         
         Paragraph pDrugs = new Paragraph("Drugs:");
+        pDrugs.setBold();
         List lDrugs = new List();
         for (PatientDrugModel dm : patientModel.getDrugs()) {
-        	lDrugs.add(dm.getDrug().getName());
+        	lDrugs.add(dm.getDrug().getName() + " - Dose: " + dm.getDose());
 		}
         doc.add(pDrugs);
         doc.add(lDrugs);
         
         Paragraph pDoctor = new Paragraph("Doctors:");
+        pDoctor.setBold();
         List lDoctors = new List();
         for (DoctorModel dm : patientModel.getDoctors()) {
         	lDoctors.add(dm.getFirstname() + " " + dm.getLastname());
 		}
         doc.add(pDoctor);
         doc.add(lDoctors);
+        
+        Paragraph pAppointment = new Paragraph("Appointments:");
+        pAppointment.setBold();
+        float [] pcWidthsAppointment = {100F, 200F, 100F, 100F, 100F};       
+        Table appoointmentTable = new Table(pcWidthsAppointment);
+        addCell(appoointmentTable, "Name", true);
+        addCell(appoointmentTable, "Description", true);
+        addCell(appoointmentTable, "Start", true);
+        addCell(appoointmentTable, "End", true);
+        addCell(appoointmentTable, "Doctor", true);
+                       
+        for (AppointmentModel am : appointmentModel) {
+        	addCell(appoointmentTable, am.getName(), false);
+            addCell(appoointmentTable, am.getDescription(), false);
+            addCell(appoointmentTable, am.getStart().format(formatter), false);
+            addCell(appoointmentTable, am.getEnd().format(formatter), false);
+            if(am.getDoctor() != null) {
+            	addCell(appoointmentTable, am.getDoctor().getFirstname() + " " + am.getDoctor().getLastname(), false);
+            }else {
+            	addCell(appoointmentTable, "", false);
+            }
+		}
+        doc.add(pAppointment);
+        doc.add(appoointmentTable);
         
         String imFile = new File("").getAbsolutePath() + "/src/main/webapp/VAADIN/themes/mytheme/img/pms_64px.png";       
         ImageData data = ImageDataFactory.create(imFile);        
@@ -131,6 +161,16 @@ public class PdfSource implements StreamSource {
         doc.add(image); 
         
         doc.close(); 
+    }
+    
+    private void addCell(Table table, String text, boolean bolt) {
+    	Cell cAp1 = new Cell();                        
+        cAp1.add(text);                              
+        cAp1.setBorder(Border.NO_BORDER);
+        if(bolt) {
+        	cAp1.setBold();
+        }
+        table.addCell(cAp1);
     }
 
     @Override
